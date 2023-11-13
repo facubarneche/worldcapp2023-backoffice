1f3233b77332410ac5c26b1f7b9d810c812ce525
alemeni2001 <alejomenini@gmail.com>
2023-11-13 20:06:19 -0300

    Actualizacion del componente NavBar para que este seleccione su icono segun la ruta

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,42 +1,64 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
-import { HomeRounded , LogoutRounded , DirectionsRunRounded , StoreRounded , AssignmentIndRounded} from '@mui/icons-material'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { HomeRounded, LogoutRounded, DirectionsRunRounded, StoreRounded, AssignmentIndRounded } from '@mui/icons-material'
 
-export const NavBar = () => {
-  const [value, setValue] = useState(0)
+const routes = [
+  { path: '/home', index: 0 },
+  { path: '/figuritas', index: 1 },
+  { path: '/jugadores', index: 2 },
+  { path: '/puntos-de-venta', index: 3 },
+  { path: '/login', index: 4 },
+]
+
+const findRouteIndex = (currentPath) => {
+  const route = routes.find(route => route.path === currentPath)
+  return route ? route.index : -1
+}
+
+const NavBar = () => {
   const navigate = useNavigate()
+  const location = useLocation()
+  
+  // Estado para mantener el índice de la ruta seleccionada
+  const [value, setValue] = useState(findRouteIndex(location.pathname))
 
+  // Manejador de cambios al seleccionar un ícono en BottomNavigation
   const handleChange = (event, newValue) => {
     setValue(newValue)
   }
-
+  
+  // Función para navegar a una ubicación (ruta)
   const goToLocation = (location) => {
     navigate(location)
   }
-  
 
-  return(
+  // Efecto secundario para actualizar el índice cuando cambia la ruta
+  useEffect(() => {
+    setValue(findRouteIndex(location.pathname))
+  }, [location.pathname])
+
+  return (
     <BottomNavigation value={value} onChange={handleChange}>
       <BottomNavigationAction
-        icon={<HomeRounded />}
+        icon={<HomeRounded sx={{ fontSize: 30 }} />}
         onClick={() => goToLocation('/home')}
       />
       <BottomNavigationAction
-        icon={<AssignmentIndRounded />}
+        icon={<AssignmentIndRounded sx={{ fontSize: 30 }} />}
         onClick={() => goToLocation('/figuritas')}
       />
       <BottomNavigationAction
-        icon={<DirectionsRunRounded />}
+        icon={<DirectionsRunRounded sx={{ fontSize: 30 }} />}
         onClick={() => goToLocation('/jugadores')}
       />
       <BottomNavigationAction
-        icon={<StoreRounded />}
+        icon={<StoreRounded sx={{ fontSize: 30 }} />}
         onClick={() => goToLocation('/puntos-de-venta')}
       />
       <BottomNavigationAction
-        icon={<LogoutRounded />}
+        icon={<LogoutRounded sx={{ fontSize: 30 }} />}
         onClick={() => goToLocation('/login')}
       />
     </BottomNavigation>
@@ -44,5 +66,6 @@ export const NavBar = () => {
 }
 
 export default NavBar
+
 
 
