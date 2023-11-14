85a2a4baab14a3dfe9db10e3c450c0454975b494
madescoces <madescoces@gmail.com>
2023-11-14 12:48:21 -0300

    Cambios en axios

--- src/domain/services/UserService/UserService.js
+++ src/domain/services/UserService/UserService.js
@@ -3,6 +3,7 @@ import { REST_SERVER_URL, USER_KEY_STORAGE } from '../constants'
 
 const UserService = () => {
   const login = async (props) => {
+    console.log(props)
     const response$ = await axios.post(`${REST_SERVER_URL}/user/login`,props)
     const userID = response$.data.userLogedID.toString()
     sessionStorage.setItem(USER_KEY_STORAGE, userID)
