af8b8dfaa947e1bd3a3998167e1c80c31fa374dd
alemeni2001 <alejomenini@gmail.com>
2023-11-15 21:15:32 -0300

    Refactor al componente NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,68 +4,46 @@ import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 import { HomeRounded, LogoutRounded, DirectionsRunRounded, StoreRounded, AssignmentIndRounded } from '@mui/icons-material'
 
-const routes = [
-  { path: '/home', index: 0 },
-  { path: '/figuritas', index: 1 },
-  { path: '/jugadores', index: 2 },
-  { path: '/puntos-de-venta', index: 3 },
-  { path: '/login', index: 4 },
-]
+const iconSize = 30
 
-const findRouteIndex = (currentPath) => {
-  const route = routes.find(route => route.path === currentPath)
-  return route ? route.index : -1
-}
+const navigationButtons = [
+  { path: '/home', icon: <HomeRounded sx={{ fontSize: iconSize }} /> },
+  { path: '/figuritas', icon: <AssignmentIndRounded sx={{ fontSize: iconSize }} /> },
+  { path: '/jugadores', icon: <DirectionsRunRounded sx={{ fontSize: iconSize }} /> },
+  { path: '/puntos-de-venta', icon: <StoreRounded sx={{ fontSize: iconSize }} /> },
+  { path: '/login', icon: <LogoutRounded sx={{ fontSize: iconSize }} /> },
+]
 
 const NavBar = () => {
   const navigate = useNavigate()
   const location = useLocation()
-  
-  // Estado para mantener el índice de la ruta seleccionada
-  const [value, setValue] = useState(findRouteIndex(location.pathname))
+  const [value, setValue] = useState(location.pathname)
 
-  // Manejador de cambios al seleccionar un ícono en BottomNavigation
   const handleChange = (event, newValue) => {
     setValue(newValue)
-  }
-  
-  // Función para navegar a una ubicación (ruta)
-  const goToLocation = (location) => {
-    navigate(location)
+    navigate(newValue)
   }
 
-  // Efecto secundario para actualizar el índice cuando cambia la ruta
   useEffect(() => {
-    setValue(findRouteIndex(location.pathname))
+    setValue(location.pathname)
   }, [location.pathname])
 
   return (
     <BottomNavigation value={value} onChange={handleChange}>
-      <BottomNavigationAction
-        icon={<HomeRounded sx={{ fontSize: 30 }} />}
-        onClick={() => goToLocation('/home')}
-      />
-      <BottomNavigationAction
-        icon={<AssignmentIndRounded sx={{ fontSize: 30 }} />}
-        onClick={() => goToLocation('/figuritas')}
-      />
-      <BottomNavigationAction
-        icon={<DirectionsRunRounded sx={{ fontSize: 30 }} />}
-        onClick={() => goToLocation('/jugadores')}
-      />
-      <BottomNavigationAction
-        icon={<StoreRounded sx={{ fontSize: 30 }} />}
-        onClick={() => goToLocation('/puntos-de-venta')}
-      />
-      <BottomNavigationAction
-        icon={<LogoutRounded sx={{ fontSize: 30 }} />}
-        onClick={() => goToLocation('/login')}
-      />
+      {navigationButtons.map((route, index) => 
+        <BottomNavigationAction
+          key={index}
+          value={route.path}
+          icon={route.icon}
+        />
+      )}
     </BottomNavigation>
   )
 }
 
 export default NavBar
+
+
 
 
 
