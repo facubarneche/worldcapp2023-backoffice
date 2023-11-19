1651a9f89c1e0106c9ea84d9feb1e892028c14e3
Facundo Barneche <fh.barneche@gmail.com>
2023-11-19 16:05:59 -0300

    refactor: Nombres metodos

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -16,22 +16,22 @@ const Error = () => {
   const is500Error = () => data.status === 500
   const is404Error = () => data.status === 404
 
-  const printMessage = () => showMessage() ? data.message : data.errorMessage
+  const getMessage = () => showMessage() ? data.message : data.errorMessage
 
-  const printText = () => isNetworkError() ? 'Actualizar' : 'Volver'
-  
+  const getButtonText = () => isNetworkError() ? 'Actualizar' : 'Volver'
+
   const pickImage = () => is500Error() || is404Error() ? data.status : 'unexpected'
 
-  const returnPage = () => {
+  const goBack = () => {
     navigate(-1)
   }
 
   return (
     <div className='display-error'>
       <h1>Oops... ha ocurrido un error</h1>
-      <img className="display-error__image" src={`/public/images/${pickImage()}-error.png`} alt={`Error ${data.message ?? data.errorMessage}`} />
-      <h2>{ printMessage() }</h2>
-      <Button className="display-error__button" onClick={returnPage}>{ printText() }</Button>
+      <img className="display-error__image" src={`/public/images/${pickImage()}-error.png`} alt={`Error ${data.message ?? data.errorMessage}`} data-testid='error-image'/>
+      <h2>{ getMessage() }</h2>
+      <Button className="display-error__button" onClick={goBack}>{ getButtonText() }</Button>
     </div>
   )
 }
