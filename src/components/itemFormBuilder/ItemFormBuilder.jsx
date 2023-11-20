c808e2b176052c188135d7772a537e826af21011
alemeni2001 <alejomenini@gmail.com>
2023-11-20 19:00:08 -0300

    Refactorizacion componente ItemFormBuilder

--- src/components/itemFormBuilder/ItemFormBuilder.jsx
+++ src/components/itemFormBuilder/ItemFormBuilder.jsx
@@ -1,48 +1,48 @@
 import { Button, TextField, Checkbox, FormControlLabel } from "@mui/material"
 import './ItemFormBuilder.css'
 
-const ItemFormBuilder = ({ inputs }) => {
-  return (
-    <div className="item-form-builder">
-      {inputs.map((input, index) => 
-        <div key={index}>
-          {input.type === 'checkbox' ? 
-            <FormControlLabel
-              control={<Checkbox defaultChecked={input.defaultValue} />}
-              label={input.label}
-              className="item-form-builder__checkbox"
-            />
-            : 
-            <TextField
-              key={index}
-              className="item-form-builder__input"
-              required
-              label={input.label}
-              type={input.type}
-              defaultValue={input.defaultValue}
-              select={input.type === 'select'}
-              SelectProps={{
-                native: true,
-              }}
-            >
-              {input.type === 'select' &&
-                input.options.map((option) => 
-                  <option key={option} value={option}>
-                    {option}
-                  </option>
-                )}
-            </TextField>
-          }
-        </div>
-      )}
-      <Button className="item-form-builder__button" variant="contained">
-        Guardar
-      </Button>
-      <Button className="item-form-builder__button" variant="outlined">
-        Volver
-      </Button>
-    </div>
-  )
+const renderInput = (input, index) => {
+  const commonProps = {
+    key: index,
+    className: input.type === 'checkbox' ? "item-form-builder__checkbox" : "item-form-builder__input",
+  }
+
+  if (input.type === 'checkbox') {
+    return <FormControlLabel control={<Checkbox defaultChecked={input.defaultValue} />} label={input.label} {...commonProps} />
+  } else {
+    return (
+      <TextField
+        required
+        label={input.label}
+        type={input.type}
+        defaultValue={input.defaultValue}
+        select={input.type === 'select'}
+        SelectProps={{ native: true }}
+        {...commonProps}
+      >
+        {input.type === 'select' &&
+          input.options.map((option) => 
+            <option key={option} value={option}>
+              {option}
+            </option>
+          )}
+      </TextField>
+    )
+  }
 }
+
+const ItemFormBuilder = ({ inputs }) => 
+  <div className="item-form-builder">
+    {inputs.map((input, index) => 
+      <div key={index}>{renderInput(input, index)}</div>
+    )}
+    <Button className="item-form-builder__button" variant="contained">
+      Guardar
+    </Button>
+    <Button className="item-form-builder__button" variant="outlined">
+      Volver
+    </Button>
+  </div>
+
 
 export default ItemFormBuilder
