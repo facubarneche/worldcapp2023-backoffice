b587111a5951d36475815eeaad3609086d168a36
Facundo Barneche <fh.barneche@gmail.com>
2023-11-25 17:53:35 -0300

    feature: Se implementa el objeto de dominio, y el service que obtiene la info para mostrar en la vista

--- src/pages/salesPoint/SalesPoint.jsx
+++ src/pages/salesPoint/SalesPoint.jsx
@@ -3,46 +3,48 @@ import SalesPointForm from "../../components/forms/salesPointForm/SalesPointForm
 import './SalesPoint.css'
 import { useState } from "react"
 import { useOnInit } from "src/customHooks/hooks"
-import { useOutletContext } from "react-router-dom"
+import { useNavigate, useOutletContext } from "react-router-dom"
+import { getSalesPoint } from "src/domain/services/salesPointService/SalesPointService"
+import HandleError from "src/utils/handleError/HandleError"
 
 const SalesPoint = () => {
-  const [displayForm, setDisplayForm] = useState(false)
+  // @ts-ignore
   const [setHeaderTitle] = useOutletContext()
+  const [salesPoint, setSalesPoint] = useState([])
+  const [displayForm, setDisplayForm] = useState(false)
+  const navigate = useNavigate()
 
   const changeDisplay = () => {
     setDisplayForm(!displayForm)
   }
   
-  useOnInit(() => {
-    setHeaderTitle('Puntos de venta')
+  useOnInit(async () => {
+    try {
+      setHeaderTitle('Puntos de venta')
+      setSalesPoint(await getSalesPoint())
+    } catch (error) {
+      HandleError(error, navigate)
+    }
   })
 
   return (
     <>
-      <h1>FIGURITAS PAGE</h1>
       {
         !displayForm &&
         // TODO: Agregar la card, el resto de componentes vienen por layout
         <>
           <TextField id="outlined-search" label="Search field" type="search" />
-          <Box className='salespoint'>
-            <section className="salespoint__section">
-              Mock
-            </section>
-            <section className="salespoint__section">
-              <strong className="salespoint__quantity">20</strong>
-              <h2>Mock</h2>
-            </section>
-          </Box>
-          <Box className='salespoint'>
-            <section className="salespoint__section">
-              Mock2
-            </section>
-            <section className="salespoint__section">
-              <strong className="salespoint__quantity">20</strong>
-              <h2>Mock2</h2>
-            </section>
-          </Box>
+          {
+            salesPoint.map(salePoint =>
+              <Box key={salePoint.id} className='card'>
+                <div>{salePoint.name}</div>
+                <div>{salePoint.type}</div>
+                <div>{salePoint.streetName}</div>
+                <div>{salePoint.streetNumber}</div>
+                <div>{salePoint.envelopeStock}</div>
+              </Box>
+            )
+          }
           <Button className="salespoint__button" onClick={changeDisplay}>+</Button>
         </>
       }
