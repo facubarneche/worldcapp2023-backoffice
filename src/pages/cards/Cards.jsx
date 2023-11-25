a29fbd56b89cc75bb2c6498b6ba664c789461842
madescoces <madescoces@gmail.com>
2023-11-25 13:51:25 -0300

    Base

--- src/pages/cards/Cards.jsx
+++ src/pages/cards/Cards.jsx
@@ -1,14 +1,12 @@
-import { useOutletContext } from "react-router-dom"
-import { useOnInit } from "src/customHooks/hooks"
+// import { useOutletContext } from "react-router-dom"
+// import { useOnInit } from "src/customHooks/hooks"
 
 export default function Cards() {
-  const [setHeaderTitle] = useOutletContext()
-  
-  useOnInit(() => {
-    setHeaderTitle('Figuritas')
-  })
+  // const [setHeaderTitle] = useOutletContext()
 
-  return (
-    <div className="cards">Cards</div>
-  )
+  // useOnInit(() => {
+  //   setHeaderTitle('Figuritas')
+  // })
+
+  return <div className="cards">Cards</div>
 }
