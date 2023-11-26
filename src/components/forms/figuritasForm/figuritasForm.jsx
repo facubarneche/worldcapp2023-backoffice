f31d3e897248802371c440fd19d9094031d44ec3
alemeni2001 <alejomenini@gmail.com>
2023-11-26 14:38:31 -0300

    Se realizo un refactor de figuritas form

--- src/components/forms/figuritasForm/figuritasForm.jsx
+++ src/components/forms/figuritasForm/figuritasForm.jsx
@@ -5,7 +5,7 @@ import { useOnInit } from "src/customHooks/hooks"
 import { cardService } from "src/domain/services/cardService/CardService"
 import HandleError from "src/utils/handleError/HandleError"
 import { useNavigate } from "react-router-dom"
-import { useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { useOutletContext } from "react-router-dom"
 import { BASE_VALUE } from "src/domain/models/Card.model"
 
@@ -14,13 +14,12 @@ const FiguritasForm = ({ changeDisplay }) => {
   const [setHeaderTitle] = useOutletContext()
   const [players, setPlayers] = useState([])
   const [printsLevel, setPrintsLevel] = useState([])
-  const [valorationBase, setValorationBase] = useState(0)
-  const [totalValoration, setTotalValoration] = useState(0)
-  const nroRef = useRef(1)
-  const onFireRef = useRef(false)
-  const printLevelRef = useRef(1)
-  const playerRef = useRef(1)
   const navigate = useNavigate()
+  // Inputs del form
+  const [nro, setNro] = useState("") // Estado para el número
+  const [isOnFire, setIsOnFire] = useState(true) // Estado para la casilla de verificación "On Fire"
+  const [selectedPrintLevel, setSelectedPrintLevel] = useState("bajo") // Estado para el nivel de impresión
+  const [valoracionBase, setValoracionBase] = useState(0)
 
   useOnInit(async () => {
     try {
@@ -28,82 +27,107 @@ const FiguritasForm = ({ changeDisplay }) => {
       const { jugadores, levelPrints } = await cardService.getDataCreateCards()
       setPlayers(jugadores)
       setPrintsLevel(levelPrints)
-      calculateBaseValoration()
     } catch (error) {
       HandleError(error, navigate)
     }
   })
-  
-  const onFireMultiplier = () => onFireRef.current.checked ? 1.2 : 1.0
-  const evenMultiplier = () => nroRef.current.value % 2 == 0 ? 1.1 : 1.0
 
-  const calculateBaseValoration = () => {
-    const baseValue = BASE_VALUE * onFireMultiplier() * evenMultiplier() * printLevelRef.current.value
-    setValorationBase(baseValue)
-    setTotalValoration(Number(playerRef.current.value) + baseValue)
-  }
+
+  const calculateBaseValoration = (nro, isOnFire, selectedPrintLevel) => {
+    const baseValue = BASE_VALUE
+    const onFireMultiplier = isOnFire ? 1.5 : 1
+    const evenMultiplier = nro % 2 === 0 ? 1.1 : 1.0
+    const printLevelMultiplier = selectedPrintLevel === "bajo" ? 1.0 : 0.85
+
+    return baseValue * onFireMultiplier * evenMultiplier * printLevelMultiplier
+ 
+  } 
+  
+  useEffect(() => {
+    // Convertir el número a un entero
+    const parsedNro = parseInt(nro, 10)
+ 
+    const calculatedValoracionBase = calculateBaseValoration(
+      parsedNro,
+      isOnFire,
+      selectedPrintLevel
+    )
+    // Actualizar el estado de la valoración base
+    setValoracionBase(calculatedValoracionBase)
+  }, [nro, isOnFire, selectedPrintLevel])
 
 
   return (
     <div className="figuritas-form">
-      <TextField 
-        required label="Nro" 
-        type="number" 
-        inputRef={nroRef} 
-        onChange={calculateBaseValoration}
+      <TextField
+        required
+        label="Nro"
+        type="number"
+        value={nro}
+        onChange={(e) => setNro(e.target.value)}
       />
 
-      <TextField className="figuritas-form__select"
+      <TextField
+        className="figuritas-form__select"
         required
         select
         SelectProps={{ native: true }}
-        inputRef={playerRef} 
-        onChange={calculateBaseValoration}
       >
-        {
-          players.map((player, index) =>
-            //TODO: Ver bien lo del value luego
-            <option key={index} value={player.valoracion} >
-              {`${player.nombre} ${player.apellido}`}
-            </option>
-          )
-        }
+        {players.map((player, index) => 
+          <option key={index} value={player.nombre}>
+            {`${player.nombre} ${player.apellido}`}
+          </option>
+        )}
       </TextField>
 
-      <FormControlLabel 
-        className="figuritas-form__checkbox" 
-        control={<Checkbox defaultChecked={false} />} 
-        label="On Fire" 
-        inputRef={onFireRef}
-        onChange={calculateBaseValoration}
+      <FormControlLabel
+        className="figuritas-form__checkbox"
+        control={
+          <Checkbox
+            checked={isOnFire}
+            onChange={(e) => setIsOnFire(e.target.checked)}
+          />
+        }
+        label="On Fire"
       />
 
-      <TextField className="figuritas-form__select"
+      <TextField
+        className="figuritas-form__select"
         required
         select
         SelectProps={{ native: true }}
-        inputRef={printLevelRef} 
-        onChange={calculateBaseValoration}
+        value={selectedPrintLevel}
+        onChange={(e) => setSelectedPrintLevel(e.target.value)}
       >
-        {
-          printsLevel.map((printLevel) =>
-          //TODO: Ver value aca, el back seguramente necesite el nombre
-            <option key={printLevel.nombre} value={printLevel.afectaValorEn}>
-              {printLevel.nombre}
-            </option>
-          )
-        }
+        {printsLevel.map((printLevel) => 
+          <option key={printLevel.nombre} value={printLevel.afectaValorEn}>
+            {printLevel.nombre}
+          </option>
+        )}
       </TextField>
 
-      <TextField className="figuritas-form__input" required label="Imagen" type="text" />
+      <TextField
+        className="figuritas-form__input"
+        required
+        label="Imagen"
+        type="text"
+      />
 
-      <strong>Valoración base {valorationBase.toFixed(2)}</strong>
-      <strong>Valoración total {totalValoration}</strong>
+      {/* Mostrar la valoración base actualizada */}
+      <strong>Valoración base {valoracionBase}</strong>
+      <strong>Valoración total {1000}</strong>
 
-      <Button variant="contained" onClick={changeDisplay}>Guardar</Button>
-      <Button variant="outlined" onClick={changeDisplay}>Volver</Button>
+      {/* totalValoration = () => this.valoration + this.baseValoration() */}
+
+      <Button variant="contained" onClick={changeDisplay}>
+      Guardar
+      </Button>
+      <Button variant="outlined" onClick={changeDisplay}>
+      Volver
+      </Button>
     </div>
   )
+
 }
 
 export default FiguritasForm
