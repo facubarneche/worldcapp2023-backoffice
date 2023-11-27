e53c491f55388d03b02514c4616587ab30941046
JuanCaceffo <caceffojuan@gmail.com>
2023-11-27 17:37:17 -0300

    finalizacion de biding de inputs de form

--- src/pages/FormPlayer/FormPlayer.jsx
+++ src/pages/FormPlayer/FormPlayer.jsx
@@ -2,47 +2,138 @@ import { useOutletContext } from 'react-router-dom'
 import FormActions from 'src/components/FormActions/FormActions'
 import { useOnInit } from 'src/customHooks/hooks'
 import './FormPlayer.css'
-import { TextField, Typography } from '@mui/material'
-import { useState } from 'react'
+import {
+  Checkbox,
+  FormControlLabel,
+  MenuItem,
+  TextField,
+  Typography,
+} from '@mui/material'
+import { useEffect, useState } from 'react'
 
 const FormPlayer = ({ headerTitle }) => {
   // @ts-ignore
   const [setHeaderTitle] = useOutletContext()
-  const [playerData, setPlayerData] = useState({})
+  const [playerData, setPlayerData] = useState({
+    nombre: undefined,
+    apellido: undefined,
+    nacimiento: undefined,
+    altura: undefined,
+    peso: undefined,
+    camiseta: undefined,
+    seleccion: '',
+    debut: undefined,
+    posicion: '',
+  })
+  //TODO: realizar
+  const [nationalTeamOptions, setNationalTeamOptions] = useState([
+    'Argentina',
+    'Brazil',
+  ])
+  const positions = [
+    'Arquero',
+    'Delantero',
+    'Mediocampista',
+    'Defensor',
+    'Polivalente',
+  ]
 
   useOnInit(() => {
     setHeaderTitle(headerTitle)
   })
 
+  useEffect(() => {
+    console.log(playerData)
+  })
+
+  const handleChecked = (e, key) => {
+    const value = e.target.checked
+    setPlayerData((prev) => ({ ...prev, [key]: value }))
+  }
+
   const handleChange = (e, key) => {
     const value = e.target.value
     setPlayerData((prev) => ({ ...prev, [key]: value }))
-    console.log(playerData)
   }
 
-  const inputData = [
+  const inputsData = [
+    { textLabel: 'Nombre', key: 'nombre', props: {} },
+    { textLabel: 'Apellido', key: 'apellido', props: {} },
     {
-      label: 'Nombre',
-      inputElement: <TextField onChange={(e) => handleChange(e, 'nombre')} />,
+      textLabel: 'Fecha de nacimineto',
+      key: 'nacimiento',
+      props: { type: 'date' },
+    },
+    { textLabel: 'Altura', key: 'altura', props: { type: 'number' } },
+    { textLabel: 'Peso', key: 'peso', props: { type: 'number' } },
+    { textLabel: 'Nro Camiseta', key: 'camiseta', props: { type: 'number' } },
+    {
+      textLabel: 'Seleccion',
+      key: 'seleccion',
+      props: {
+        select: true,
+      },
+      children: nationalTeamOptions.map((nationalTeam) => (
+        <MenuItem key={nationalTeam} value={nationalTeam}>
+          {nationalTeam}
+        </MenuItem>
+      )),
     },
     {
-      label: 'Apellido',
-      inputElement: <TextField onChange={(e) => handleChange(e, 'apellido')} />,
+      textLabel: 'Año de debut en la seleccion',
+      key: 'debut',
+      props: {
+        type: 'date',
+      },
     },
+    {
+      textLabel: 'Posicion',
+      key: 'posicion',
+      props: {
+        select: true,
+      },
+      children: positions.map((position) => (
+        <MenuItem key={position} value={position}>
+          {position}
+        </MenuItem>
+      )),
+    },
+    {
+      inputElement: (
+        <FormControlLabel
+          control={
+            <Checkbox
+              value={playerData['esLider']}
+              onChange={(e) => handleChecked(e, 'esLider')}
+            />
+          }
+          label="Es Lider"
+        />
+      ),
+    },
+    { textLabel: 'Cotizacion', key: 'cotizacion', props: { type: 'number' } },
   ]
 
   return (
     <main className="formPlayer formPlayer__flexContainer">
       <form className="formPlayer__form">
-        {inputData.map((data, index) => (
+        {inputsData.map((data, index) => (
           <section
             className="formPlayer__input-container formPlayer__flexContainer"
-            key={data.label + index}
+            key={index}
           >
             <Typography component="label" variant="h5">
-              {data.label}
+              {data.textLabel}
             </Typography>
-            {data.inputElement}
+            {data.inputElement ?? (
+              <TextField
+                value={playerData[data.key]}
+                onChange={(e) => handleChange(e, data.key)}
+                {...data.props}
+              >
+                {data.children}
+              </TextField>
+            )}
           </section>
         ))}
       </form>
