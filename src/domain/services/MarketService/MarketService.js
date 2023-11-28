a06b770870d55308283863fe290af9c470922771
madescoces <madescoces@gmail.com>
2023-11-27 23:51:27 -0300

    Para que sol este tranquila con el color rojito

--- src/domain/services/MarketService/MarketService.js
+++ src/domain/services/MarketService/MarketService.js
@@ -1,28 +1,28 @@
-import axios from "axios"
-import { REST_SERVER_URL } from "../constants"
-import { Market } from "models/MarketModel/Market.model"
+import axios from 'axios'
+import { REST_SERVER_URL } from '../constants'
+import { Market } from 'models/MarketModel/Market.model'
 
 class MarketService {
   getMarkets = async (search) => {
-    const marketsJson = await axios.get(`${REST_SERVER_URL}/puntos-de-venta`, {params: search})
-    return marketsJson.data.map(market => Market.fromJson(market))
+    const marketsJson = await axios.get(`${REST_SERVER_URL}/puntos-de-venta`, { params: search })
+    return marketsJson.data.map((market) => Market.fromJson(market))
   }
 
-  getMarketById = async (id) => {    
+  getMarketById = async (id) => {
     const marketJson = await axios.get(`${REST_SERVER_URL}/punto-de-venta/${id}`)
     return Market.fromJson(marketJson.data)
   }
 
   updateMarket = async (market) => {
-
+    console.log(market)
   }
 
   deleteMarket = async (id) => {
-    
+    console.log(id)
   }
 
   createMarket = async (market) => {
-
+    console.log(market)
   }
 }
 
