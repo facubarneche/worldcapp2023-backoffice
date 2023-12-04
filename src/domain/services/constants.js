930b7b2aee1d46717e2d91d1cd7e9f04be9c8a08
Facundo Barneche <fh.barneche@gmail.com>
2023-12-04 08:38:05 -0300

    feature: Inicio implementacion modal para editar y crear una nueva seleccion

--- src/domain/services/constants.js
+++ src/domain/services/constants.js
@@ -2,7 +2,7 @@ export const DEBUG_MODE = false
 export const REST_SERVER_URL = 'http://localhost:9000'
 export const USER_KEY_STORAGE = 'userLogedID'
 
-
+//TODO: Implementar correctamente
 export const BusinessType = {
   Kiosco: 'Kiosco',
   Libreria: 'Libreria',
--- src/pages/OneForAll/modals/TeamsModal.jsx
+++ src/pages/OneForAll/modals/TeamsModal.jsx
@@ -1,4 +1,6 @@
-import { Box, Button, Modal, Typography } from '@mui/material'
+import { Box, Button, Modal, TextField, Typography } from '@mui/material'
+import { useState } from 'react'
+import { Team } from 'src/domain/models/TeamModel/Team.model'
 
 //TODO: Luego mover o pasar a css
 const style = {
@@ -13,9 +15,51 @@ const style = {
   p: 4,
 }
 
+//TODO: mover y reutilizar
+const InputType = {
+  TextField: 'TextField',
+  Select: 'Select',
+}
+
 const TeamsModal = () => {
   const handleClose = () => false
+  const [team, setTeam] = useState(new Team())
 
+  //   const fields = {
+  //     nombre: {
+  //       name: 'Nombre',
+  //       type: InputType.TextField,
+  //       value: team.nombre,
+  //       className: 'field',
+  //       elementProps: {},
+  //       options: {},
+  //     },
+  //     confederacion: {
+  //       name: 'Pedidos Pendientes',
+  //       type: InputType.Select,
+  //       value: team.tipoPuntoDeVenta,
+  //       className: 'field',
+  //       elementProps: { native: true },
+  //       options: ['CONMEBOL', 'UEFA'], //TODO: traer del back las confederaciones
+  //     },
+  //     copasDelMundo: {
+  //       name: 'Pedidos Pendientes',
+  //       type: InputType.TextField,
+  //       value: team.pedidosPendientes,
+  //       className: 'field',
+  //       elementProps: { type: 'number', min: 0 },
+  //       options: {},
+  //     },
+  //     copasConfederacion: {
+  //       name: 'Pedidos Pendientes',
+  //       type: InputType.TextField,
+  //       value: team.pedidosPendientes,
+  //       className: 'field',
+  //       elementProps: { type: 'number', min: 0 },
+  //       options: {},
+  //     }
+  //   }
+  
   return (
     <>
       <Button>Open modal</Button>
@@ -26,12 +70,19 @@ const TeamsModal = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-      Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <TextField required label="Nombre" type="text" value={""} />
+          <TextField
+            className="figuritas-form__select"
+            required
+            select
+            SelectProps={{ native: true }}
+          >
+            <option>lala</option>
+            <option>lele</option>
+            <option>lolo</option>
+          </TextField>
+          <TextField required label="Copas del mundo" type="number" value={0} />
+          <TextField required label="Copas confederacion" type="number" value={0} />
         </Box>
       </Modal>
     </>
