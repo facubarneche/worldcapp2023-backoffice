117294391d3f3d40fbb7b73b13b6e370aa366b8b
madescoces <madescoces@gmail.com>
2023-12-03 21:25:39 -0300

    Se implementa un validador de inputs polimorfico que soporta distintas condiciones de validación

--- src/utils/Validators/ValidateInputs.js
+++
@@ -1,9 +0,0 @@
-export class ValidateInputs {
-  constructor() {
-    // Puedes inicializar cualquier configuración de validación aquí
-  }
-
-  static isEmpty = (value) => String(value).trim() === ''
-
-  static isAddress = (value) => /.*\s\d+$/.test(value)
-}
